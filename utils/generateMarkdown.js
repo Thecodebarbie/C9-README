10b6978b90fac88ea67600d1633db1d60a994a86
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-04-29 01:05:42 -0400

    test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -27,7 +27,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license) {
+  if (license !== 'No License') {
     console.log(`Rendering license section for license: ${license}`);
     return `
 ## License
